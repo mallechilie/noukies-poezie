70ab04e1c65710ea081b4a57f36204b4c7af633e
Michiel <michiel.doorn@gmail.com>
2024-12-18 21:43:17 +0100

    Under construction message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import ProductList from "@/components/dbTesters/ProductList";
 import ContentWidth from "@/components/wrappers/ContentWidth";
 import { getDbInfo } from "@/database/api/getDbInfo";
+import MuiMarkdown from "mui-markdown";
 import { use } from "react";
 
 export default function Page() {
@@ -10,6 +11,9 @@ export default function Page() {
     <>
       <Header title="Nouks Poëzie" height={400} width={1440} />
       <ContentWidth>
+        <MuiMarkdown>
+          {`##### De website is nog in constructie, je kan me bereiken op <nouks_poezie@outlook.com>`}
+        </MuiMarkdown>
         <ProductList />
       </ContentWidth>
     </>
