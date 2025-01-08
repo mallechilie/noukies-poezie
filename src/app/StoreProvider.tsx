eecd27ca0e5ca014b5912f4a72a851eade023b17
Michiel <michiel.doorn@gmail.com>
2025-01-08 17:30:14 +0100

    Persist reducers, add type issues

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,12 +2,19 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "@/lib/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
-  
-  return <Provider store={storeRef.current}>{children}</Provider>;
+
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={storeRef.current.__persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketSlice from "@/lib/features/basket/basketSlice";
+import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage"; 
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "persist",
+  storage
+};
+
+const rootReducer = combineReducers({
+  basket: basketSlice
+});
+
+const makeConfiguredStore = () =>
+  configureStore({
+    reducer: rootReducer,
+  })
+
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      basket: basketSlice
-    }
-  });
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return makeConfiguredStore();
+  } else {
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+    const store: any = configureStore({
+      reducer: persistedReducer
+    });
+    store.__persistor = persistStore(store);
+    return store;
+  }
 };
 
 // Infer the type of makeStore
